30b3b64db1ae116c93bb2c7953984425556664ff
choonkeun <choonkeun@yahoo.com>
2023-01-19 14:16:22 -0800

    First push

---
+++ ConsoleApp1/ConsoleApp1/Azure/KeyVaultManager.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+
+namespace ConsoleApp1.Azure
+{
+    public class KeyVaultManager
+    {
+        readonly string tenantId;
+        readonly string clientId;
+        readonly string clientSecret;
+        readonly string kvUri;
+        public KeyVaultManager() 
+        {
+            tenantId = "07377ea2-4656-4388-92c3-da76edd31ef4";
+            clientId = "087e8e27-5a2a-40c3-b8a9-73a8fea86ced";
+            clientSecret = "n5X8Q~yZoPdTqVZ75nGE6HdNh4zITagLJwMjvdjG";
+            kvUri = "https://*****.vault.azure.net";
+        }
+
+        public string GetSecret(string SecretName)
+        {
+            var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            var client = new SecretClient(new Uri(kvUri), credential);
+            return client.GetSecret(SecretName).Value.Value;
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1.Azure;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Key Vault access using .NET Client with .NET Core 3.1");
+
+            KeyVaultManager keyVaultmanager = new KeyVaultManager();
+
+            Console.WriteLine("my-test-secret-1: {0}", keyVaultmanager.GetSecret("my-test-secret-1"));
+            Console.WriteLine("BookStores: {0}", keyVaultmanager.GetSecret("BookStores"));
+
+            Console.ReadLine();
+        }
+    }
+}
+
